7f2676c5e8afd5286d80437634cd60329effb5b6
jobayer58 <jobayerafnan770@gmail.com>
2025-01-07 18:25:19 +0600

    update

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,36 @@
+import { FaBookmark } from 'react-icons/fa';
 
-
-const Blog = () => {
+const Blog = ({ blog, handleAddToBookmark,handleMarkAsRead }) => {
+    const { title, cover, author, author_img, posted_date, reading_time, hashtags } = blog
     return (
-        <div>
-            
+        <div className="mb-20 space-y-4">
+            <img className="w-full mb-8" src={cover} alt={`cover picture of the title ${title}`} />
+            <div className="flex justify-between mb-4">
+                <div className="flex">
+                    <img className="w-14" src={author_img} alt="" />
+                    <div className="ml-6">
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button
+                        onClick={() => handleAddToBookmark(blog)}
+                        className='ml-2 text-2xl text-red-600'
+                    ><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
+            <button 
+            onClick={() => handleMarkAsRead(reading_time)}
+            className='text-purple-800 font-bold underline'
+            >Mark As Read</button>
         </div>
     );
 };
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,16 @@
+import Bookmark from "../Bookmark/Bookmark";
 
 
-const Bookmarks = () => {
+const Bookmarks = ({bookmarks, readingTime}) => {
     return (
-        <div className="md:w-1/3">
-            <h2>Bookmarks</h2>
+        <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4">
+            <div>
+                <h3 className="text-4xl">Reading time: {readingTime}</h3>
+            </div>
+            <h2 className="text-3xl text-center ">Bookmarks Blogs: {bookmarks.length}</h2>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+            }
         </div>
     );
 };
